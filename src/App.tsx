5128f990970f62e7708dae54ed65e1b33c8f4751
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2024-03-15 12:19:32 +0300

    testestest

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import TestComp from './components/TestComp';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
